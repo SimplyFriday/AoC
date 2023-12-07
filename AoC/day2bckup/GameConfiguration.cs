b79c209d5bad11e7a7227a1375ba0f18b69ac131
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2023-12-06 22:43:39 -0500

    removed accident - Working solution!!!

--- AoC/day2bckup/GameConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AoC.Day02
-{
-    internal class GameConfiguration
-    {
-        public int MaxRedCount { get; set; }
-        public int MaxGreenCount { get; set; }
-        public int MaxBlueCount { get; set; }
-    }
-}
