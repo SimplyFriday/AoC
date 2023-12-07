90d65973b5737ca33efde1d4e41632a324c08fa7
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2023-12-07 14:53:51 -0500

    wrote powerOfLowestPossible() for part2

--- AoC/Day02/GameIteration.cs
+++ AoC/Day02/GameIteration.cs
@@ -24,7 +24,30 @@
             return true;
         }
 
-        public record GameRound
+        public int powerOfLowestPossible() {
+            GameRound lowestCounts = new GameRound
+            {
+                RedCount = int.MaxValue,
+                BlueCount = int.MaxValue,
+                GreenCount = int.MaxValue
+            };
+
+            foreach(GameRound round in Rounds) {
+                lowestCounts.RedCount = round.RedCount < lowestCounts.RedCount ?
+		                                round.RedCount : lowestCounts.RedCount;
+
+                lowestCounts.BlueCount = round.BlueCount < lowestCounts.BlueCount ?
+		                                 round.BlueCount : lowestCounts.BlueCount;
+
+                lowestCounts.GreenCount = round.GreenCount < lowestCounts.GreenCount ?
+		                                  round.GreenCount : lowestCounts.GreenCount;
+	        }
+            //called power bc thats what the puzzle called it
+            int power = lowestCounts.RedCount * lowestCounts.BlueCount * lowestCounts.GreenCount;
+            return power;
+	    }
+        
+        public struct GameRound
         {
             public int RedCount { get; set; }
             public int BlueCount { get; set; }
